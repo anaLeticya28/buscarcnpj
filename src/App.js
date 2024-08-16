5f342bebcca97b193d2adc36c5822bb350f715c2
Ana leticya <ana.l.albuquerque@aluno.senai.br>
2024-08-16 16:23:03 -0300

    primeira versao

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// bloco de importações
+
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import './style.css';
+import api from "./services/api"
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [CNPJ, setCNPJ] = useState({});
+
+  
+  // função assíncrona
+  async function handleSearch(){
+    
+    if(input === ''){
+      alert("Preencha algum CNPJ!")
+      return;
+    }
+
+
+    // tratativa de erros
+    try{
+      const response = await api.get(`${input}`)
+      setCNPJ(response.data)
+      setInput("")
+    }catch{
+      alert("Erro ao buscar CNPJ!")
+      setInput("")
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // estrutura HTML para preenchimento
+    <div className="container">
+      <h1 className="title">Consultar CNPJ</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite o CNPJ..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+      {/* Validação de NPJ e desconstrução de objeto JSON */}
+
+      {Object.keys(CNPJ).length > 0 && (
+        <main className="main">
+          <h2>razao social: {CNPJ.razao_social}</h2>
+          <span>fundaçao:{CNPJ.data_inicio_atividade}</span>
+          <span>situação cadastral: {CNPJ.descricão_situacao_cadastral}</span>
+          <span>contato: {CNPJ.ddd_fax}</span>
+        </main>
+      )} 
+
     </div>
   );
 }
